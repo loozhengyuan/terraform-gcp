f4817af5eaac1a94f22c955baf730b1759177080
Loo Zheng Yuan <loozhengyuan@users.noreply.github.com>
2020-03-16 15:36:25 +0800

    switch locals to input vars

--- modules/gke_cluster_workload_identity/main.tf
+++ modules/gke_cluster_workload_identity/main.tf
@@ -6,12 +6,6 @@ terraform {
   }
 }
 
-locals {
-  ksa_namespace = "workload-identity"
-  ksa_name      = "workload-identity"
-  gsa_name      = "workload-identity"
-}
-
 # Enable IAM Service Account Credentials API
 resource "google_project_service" "container" {
   project = var.project
@@ -26,7 +20,7 @@ resource "google_project_service" "container" {
 # Create Kubernetes namespace
 resource "kubernetes_namespace" "namespace" {
   metadata {
-    name = local.ksa_namespace
+    name = var.namespace
     labels = {
       managed_by = "terraform"
     }
@@ -38,8 +32,8 @@ resource "google_service_account" "service_account" {
   provider = google
   project  = var.project
 
-  account_id   = local.gsa_name
-  display_name = local.gsa_name
+  account_id   = var.gsa_name
+  display_name = var.gsa_name
   description  = "Service account for Workload Identity."
 }
 
@@ -56,8 +50,8 @@ resource "google_project_iam_member" "iam_roles" {
 # Add annotation for KSA-GSA binding
 resource "kubernetes_service_account" "service_account" {
   metadata {
-    name      = local.ksa_name
-    namespace = local.ksa_namespace
+    name      = var.ksa_name
+    namespace = var.namespace
     labels = {
       managed_by = "terraform"
     }
@@ -72,5 +66,5 @@ resource "google_service_account_iam_member" "gsa_ksa_iam_policy_binding" {
   service_account_id = google_service_account.service_account.name
 
   role   = "roles/iam.workloadIdentityUser"
-  member = "serviceAccount:${var.project}.svc.id.goog[${local.ksa_namespace}/${local.ksa_name}]"
+  member = "serviceAccount:${var.project}.svc.id.goog[${var.namespace}/${var.ksa_name}]"
 }
--- modules/gke_cluster_workload_identity/variables.tf
+++ modules/gke_cluster_workload_identity/variables.tf
@@ -2,3 +2,18 @@ variable "project" {
   description = "The name of the Google Cloud project."
   type        = string
 }
+
+variable "namespace" {
+  description = "The name of the Kubernetes namespace to deploy to."
+  type        = string
+}
+
+variable "ksa_name" {
+  description = "The name of the Kubernetes Service Account."
+  type        = string
+}
+
+variable "gsa_name" {
+  description = "The name of the Google Service Account."
+  type        = string
+}
