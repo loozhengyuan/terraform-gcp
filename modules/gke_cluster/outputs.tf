ddcd450a1c0687d40bf9b4054bbf0aac5e747cec
Loo Zheng Yuan <loozhengyuan@users.noreply.github.com>
2020-03-16 15:04:35 +0800

    remove node_pool variable output because that is accessible using the cluster variable

--- modules/gke_cluster/outputs.tf
+++ modules/gke_cluster/outputs.tf
@@ -2,8 +2,3 @@ output "cluster" {
   value       = google_container_cluster.cluster
   description = "The Kubernetes cluster."
 }
-
-output "node_pools" {
-  value       = google_container_node_pool.node_pool
-  description = "The Kubernetes node pools associated with the cluster."
-}
