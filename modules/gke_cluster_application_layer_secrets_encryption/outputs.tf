1483523ee80993932a0f373bfb51f713c616204a
Loo Zheng Yuan <loozhengyuan@users.noreply.github.com>
2020-03-30 11:54:40 +0800

    fix missing module namespace for outputs

--- modules/gke_cluster_application_layer_secrets_encryption/outputs.tf
+++ modules/gke_cluster_application_layer_secrets_encryption/outputs.tf
@@ -1,9 +1,9 @@
 output "keyring" {
   description = "The attributes of Cloud KMS Crypto Key Ring."
-  value       = kms_keys.keyring
+  value       = module.kms_keys.keyring
 }
 
 output "key" {
   description = "The attributes of Cloud KMS Crypto Key."
-  value       = kms_keys.key
+  value       = module.kms_keys.key
 }
